075ebde5ae28d704b35b4e7c0f9ddf8c7ad7e168
axsbrad <75226829+axsbrad@users.noreply.github.com>
2021-01-26 17:22:10 -0500

    Add project files.

---
+++ MetabolonRssChallenge/Feed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetabolonRssChallenge
+{
+    //Information used to determine Feeds to check
+    public class Feed
+    {    
+        public string Company { get; set; }
+        public string URL { get; set; }
+    }
+}
---
+++ NUnitTestProject/UnitTest1.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using MetabolonRssChallenge;
+
+namespace NUnitTestProject
+{
+    public class Tests
+    {
+        public List<Feed> MockFeeds
+        {
+            get
+            {
+                List<Feed> data = new List<Feed>()
+                {
+                    new Feed{Company="Joe Rogan", URL="http://joeroganexp.joerogan.libsynpro.com/rss"},
+                    new Feed{Company="NYTimes 1619", URL="https://feeds.simplecast.com/6HzeyO6b"},
+                    new Feed{Company="NPR Code Switch", URL="https://feeds.npr.org/510312/podcast.xml"},
+                    new Feed{Company="Crime Junkies", URL="https://feeds.megaphone.fm/ADL9840290619"},
+                    new Feed{Company="The Daily by The New York Times", URL="https://feeds.simplecast.com/54nAGcIl"}
+                };
+
+                return data;
+            }
+        }
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestRssFeedEngine()
+        {
+            RssFeedEngine rssFeedEngine = new RssFeedEngine(MockFeeds, false);
+
+            var  result =  rssFeedEngine.GetFeedData();
+
+           if(result.Count == 2) Assert.Pass();
+            Assert.Fail($"result.Count <> 2 ({result.Count})");
+
+        }
+    }
+}
